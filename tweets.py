65504ab02820458276fd6c0f57f55a5786ddf088
Mamata Shrestha <mamatashrestha36@gmail.com>
2023-01-18 11:26:08 -0600

    initial commit

---
+++ tweets.py
@@ -0,0 +1,26 @@
+
+# https://github.com/mehranshakarami/AI_Spectrum/blob/main/2022/snscrape/tweets.py
+
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# query = "(from:elonmusk) until:2020-01-01 since:2010-01-01"
+query = "ronaldo lang:en until:2022-12-20 since:2022-11-1"
+tweets = []
+limit = 5000
+
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+    
+    # print(vars(tweet))
+    # break
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.user.username, tweet.content])
+        
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+print(df)
+
+# to save to csv
+df.to_csv('tweets_ronaldo.csv')
